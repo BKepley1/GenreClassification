8d21d888465153dcf628e26cf2e181568a364371
Brandon Kepley <BKepley1@users.noreply.github.com>
2020-12-06 15:16:20 -0500

    Initial commit

---
+++ GenreClassification.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os, json, math, librosa
+
+dataPath = os.getcwd()+'\\Data\\genres_original'
+songLocations = []
+genreLabels = []
+for root, dirs, files in os.walk(dataPath):
+    for name in files:
+        filename = os.path.join(root, name)
+        if filename != '/Data/genres_original/jazz/jazz.00054.wav':
+            songLocations.append(filename)
+            genreLabels.append(filename.split('\\')[8])
+
+print(set(genreLabels))
